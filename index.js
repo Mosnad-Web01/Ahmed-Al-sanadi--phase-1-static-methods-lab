ae0d0f0236e50eed9da0cb73c6c339ba51bcfa40
ahmedalsanadi <ahmed11alsanadi11@gmail.com>
2024-08-26 15:08:38 +0300

    completed

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+  static exceptions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9-' ]/g, '');
+  }
+
+  static titleize(sentence) {
+    const words = sentence.toLowerCase().split(" ");
+    
+    return words
+      .map((word, index) => {
+        if (index === 0 || !this.exceptions.includes(word)) {
+          return this.capitalize(word);
+        }
+        return word;
+      })
+      .join(" ");
+  }
 }
